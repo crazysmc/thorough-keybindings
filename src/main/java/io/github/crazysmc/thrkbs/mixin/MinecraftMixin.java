bc9a046b1721b9eabaacb793b2a6d3eab4ec60af
smc <crazysmc@users.noreply.github.com>
2024-01-15 15:37:48 +0100

    mixin slice for more precise injection

--- src/main/java/io/github/crazysmc/thrkbs/mixin/MinecraftMixin.java
+++ src/main/java/io/github/crazysmc/thrkbs/mixin/MinecraftMixin.java
@@ -27,21 +27,25 @@ public abstract class MinecraftMixin implements Runnable, SnooperPopulator
     return ThoroughKeybindings.getRemap(constant);
   }
 
-  @ModifyConstant(method = "tick", constant = {
-      @Constant(intValue = Keyboard.KEY_ESCAPE, ordinal = 3),
-      @Constant(intValue = Keyboard.KEY_0),
-      @Constant(intValue = Keyboard.KEY_T),
-      @Constant(intValue = Keyboard.KEY_A, ordinal = 0),
-      @Constant(intValue = Keyboard.KEY_S),
-      @Constant(intValue = Keyboard.KEY_F),
-      @Constant(intValue = Keyboard.KEY_LSHIFT),
-      @Constant(intValue = Keyboard.KEY_RSHIFT),
-      @Constant(intValue = Keyboard.KEY_F1),
-      @Constant(intValue = Keyboard.KEY_F3),
-      @Constant(intValue = Keyboard.KEY_F5),
-      @Constant(intValue = Keyboard.KEY_F8),
-      @Constant(intValue = Keyboard.KEY_F11),
-  })
+  @ModifyConstant(method = "tick",
+                  slice = @Slice(from = @At(value = "INVOKE",
+                                            target = "Lorg/lwjgl/input/Keyboard;next()Z",
+                                            remap = false)),
+                  constant = {
+                      @Constant(intValue = Keyboard.KEY_ESCAPE, ordinal = 0),
+                      @Constant(intValue = Keyboard.KEY_0),
+                      @Constant(intValue = Keyboard.KEY_T),
+                      @Constant(intValue = Keyboard.KEY_A, ordinal = 0),
+                      @Constant(intValue = Keyboard.KEY_S),
+                      @Constant(intValue = Keyboard.KEY_F),
+                      @Constant(intValue = Keyboard.KEY_LSHIFT),
+                      @Constant(intValue = Keyboard.KEY_RSHIFT),
+                      @Constant(intValue = Keyboard.KEY_F1),
+                      @Constant(intValue = Keyboard.KEY_F3),
+                      @Constant(intValue = Keyboard.KEY_F5),
+                      @Constant(intValue = Keyboard.KEY_F8),
+                      @Constant(intValue = Keyboard.KEY_F11),
+                  })
   private int tickRemap(int constant)
   {
     return ThoroughKeybindings.getRemap(constant);
