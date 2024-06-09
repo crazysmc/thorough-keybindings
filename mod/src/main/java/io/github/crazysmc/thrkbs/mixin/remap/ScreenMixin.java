5d6e7fe62dd270666974cdfcb3460c18a93492a7
smc <crazysmc@users.noreply.github.com>
2024-06-10 01:02:07 +0200

    fix JIJ dependency, remove old mod folder

--- mod/src/main/java/io/github/crazysmc/thrkbs/mixin/remap/ScreenMixin.java
+++
@@ -1,8 +0,0 @@
-package io.github.crazysmc.thrkbs.mixin.remap;
-
-import net.minecraft.client.gui.screens.Screen;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Screen.class)
-public abstract class ScreenMixin
-{}
