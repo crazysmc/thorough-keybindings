cd08d8ee44c6b02033f4f063b0abd96c0cd5a964
smc <crazysmc@users.noreply.github.com>
2024-07-23 14:03:48 +0200

    1.6 moved the legacy categorised option menu

---
+++ modules/options-screens/options-screens-before-13w17a/src/main/java/io/github/crazysmc/thrkbs/optionsscreens/CategoryRegistry.java
@@ -0,0 +1,53 @@
+package io.github.crazysmc.thrkbs.optionsscreens;
+
+import io.github.crazysmc.thrkbs.core.api.HardcodedMapping;
+
+import java.util.*;
+
+import static io.github.crazysmc.thrkbs.core.ThoroughKeybindings.MAPPING_REGISTRY;
+
+public class CategoryRegistry
+{
+  private final Set<String> categories = new LinkedHashSet<>();
+  private final Map<String, String> categoryMap = new HashMap<>();
+
+  public CategoryRegistry()
+  {
+    categories.add("key.categories.movement");
+    categories.add("key.categories.gameplay");
+    categories.add("key.categories.inventory");
+    categories.add("key.categories.multiplayer");
+    categories.add("key.categories.misc");
+    categoryMap.put("key.attack", "key.categories.gameplay");
+    categoryMap.put("key.back", "key.categories.movement");
+    categoryMap.put("key.chat", "key.categories.multiplayer");
+    categoryMap.put("key.command", "key.categories.multiplayer");
+    categoryMap.put("key.drop", "key.categories.inventory");
+    categoryMap.put("key.forward", "key.categories.movement");
+    categoryMap.put("key.inventory", "key.categories.inventory");
+    categoryMap.put("key.jump", "key.categories.movement");
+    categoryMap.put("key.left", "key.categories.movement");
+    categoryMap.put("key.pickItem", "key.categories.gameplay");
+    categoryMap.put("key.playerlist", "key.categories.multiplayer");
+    categoryMap.put("key.right", "key.categories.movement");
+    categoryMap.put("key.sneak", "key.categories.movement");
+    categoryMap.put("key.use", "key.categories.gameplay");
+    categoryMap.put("key.fog", "key.categories.gameplay");
+    for (HardcodedMapping mapping : MAPPING_REGISTRY.getRegisteredMappings())
+    {
+      String category = mapping.getCategory();
+      categories.add(category);
+      categoryMap.put(mapping.getName(), category);
+    }
+  }
+
+  public List<String> getCategories()
+  {
+    return new ArrayList<>(categories);
+  }
+
+  public String getCategory(String name)
+  {
+    return categoryMap.getOrDefault(name, "key.categories.misc");
+  }
+}
---
+++ modules/options-screens/options-screens-before-13w17a/src/main/java/io/github/crazysmc/thrkbs/optionsscreens/mixin/ControlsOptionsScreenMixin.java
@@ -0,0 +1,89 @@
+package io.github.crazysmc.thrkbs.optionsscreens.mixin;
+
+import io.github.crazysmc.thrkbs.optionsscreens.ControlsOptionsSubScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.resource.language.I18n;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ControlsOptionsScreen.class)
+public abstract class ControlsOptionsScreenMixin extends Screen
+{
+  @Shadow
+  protected String title;
+
+  @Inject(method = "init", at = @At("RETURN"))
+  private void changeTitle(CallbackInfo ci)
+  {
+    ControlsOptionsScreen instance = (ControlsOptionsScreen) (Object) this;
+    if (instance instanceof ControlsOptionsSubScreen)
+      title = I18n.translate(((ControlsOptionsSubScreen) instance).getCategory());
+  }
+
+  @Redirect(method = { "init", "buttonClicked", "render" },
+            at = @At(value = "FIELD",
+                     target = "Lnet/minecraft/client/options/GameOptions;keyBindings:[Lnet/minecraft/client/options/KeyBinding;",
+                     args = "array=length"))
+  private int getKeyBindingsLength(KeyBinding[] keyBindings)
+  {
+    ControlsOptionsScreen instance = (ControlsOptionsScreen) (Object) this;
+    if (instance instanceof ControlsOptionsSubScreen)
+      return ((ControlsOptionsSubScreen) instance).getKeyMappings().size();
+    return keyBindings.length;
+  }
+
+  @Redirect(method = "render",
+            at = @At(value = "FIELD",
+                     target = "Lnet/minecraft/client/options/GameOptions;keyBindings:[Lnet/minecraft/client/options/KeyBinding;",
+                     args = "array=get"))
+  private KeyBinding getKeyBinding(KeyBinding[] keyBindings, int i)
+  {
+    ControlsOptionsScreen instance = (ControlsOptionsScreen) (Object) this;
+    if (instance instanceof ControlsOptionsSubScreen)
+      return ((ControlsOptionsSubScreen) instance).getKeyMappings().get(i);
+    return keyBindings[i];
+  }
+
+  @Redirect(method = { "init", "buttonClicked", "mouseClicked", "keyPressed", "render" },
+            at = @At(value = "INVOKE",
+                     target = "Lnet/minecraft/client/options/GameOptions;getOptionName(I)Ljava/lang/String;"))
+  private String getOptionName(GameOptions options, int i)
+  {
+    ControlsOptionsScreen instance = (ControlsOptionsScreen) (Object) this;
+    if (instance instanceof ControlsOptionsSubScreen)
+      return GameOptions.getKeyName(((ControlsOptionsSubScreen) instance).getKeyMappings().get(i).keyCode);
+    return options.getOptionName(i);
+  }
+
+  @Redirect(method = "render",
+            at = @At(value = "INVOKE",
+                     target = "Lnet/minecraft/client/options/GameOptions;translate(I)Ljava/lang/String;"))
+  private String translate(GameOptions options, int i)
+  {
+    ControlsOptionsScreen instance = (ControlsOptionsScreen) (Object) this;
+    if (instance instanceof ControlsOptionsSubScreen)
+      return I18n.translate(((ControlsOptionsSubScreen) instance).getKeyMappings().get(i).name);
+    return options.translate(i);
+  }
+
+  @Redirect(method = { "mouseClicked", "keyPressed" },
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/options/GameOptions;setKeyBinding(II)V"))
+  private void setKeyBinding(GameOptions options, int i, int keyCode)
+  {
+    ControlsOptionsScreen instance = (ControlsOptionsScreen) (Object) this;
+    if (instance instanceof ControlsOptionsSubScreen)
+    {
+      ((ControlsOptionsSubScreen) instance).getKeyMappings().get(i).keyCode = keyCode;
+      options.save();
+      return;
+    }
+    options.setKeyBinding(i, keyCode);
+  }
+}
