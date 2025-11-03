ed0dc0e46c5a35d09383b8263f0935b7356aedd5
smc <crazysmc@users.noreply.github.com>
2025-11-03 11:46:49 +0100

    more tests

--- src/test/java/io/github/crazysmc/thrkbs/DynamicTextReplacerTest.java
+++ src/test/java/io/github/crazysmc/thrkbs/DynamicTextReplacerTest.java
@@ -17,10 +17,20 @@ class DynamicTextReplacerTest
         arguments("", "", "", ""),
         arguments("Tab + R = Reload chunks", "F3 + A = Reload chunks", "Tab", "r"),
         arguments("Tab + C = Copy location as /tp command, hold Tab + C to crash the game",
-                     "F3 + C = Copy location as /tp command, hold F3 + C to crash the game", "Tab", "c"),
+                  "F3 + C = Copy location as /tp command, hold F3 + C to crash the game", "Tab", "c"),
         arguments("Tab + Caps Lock = Pause without pause menu (if pausing is possible)",
-                     "F3 + Esc = Pause without pause menu (if pausing is possible)", "Tab", "Caps Lock"),
+                  "F3 + Esc = Pause without pause menu (if pausing is possible)", "Tab", "Caps Lock"),
         arguments("sʞunɥɔ pɐoꞁǝᴚ = Ɐ + ƐℲ", "sʞunɥɔ pɐoꞁǝᴚ = Ɐ + ƐℲ", "ƐℲ", "a")
+    );
+  }
+
+  public static Stream<Arguments> debugCharts()
+  {
+    return Stream.of(
+        arguments("", "", "", "", "", ""),
+        arguments("Debug charts: [Tab+A] Profiler hidden; [Tab+B] FPS + TPS hidden; [Tab+C] Ping hidden",
+                  "Debug charts: [F3+1] Profiler hidden; [F3+2] FPS + TPS hidden; [F3+3] Ping hidden",
+                  "Tab", "a", "b", "c")
     );
   }
 
@@ -29,5 +39,12 @@ class DynamicTextReplacerTest
   void debugHelpMessage(String expected, String text, String f3, String key)
   {
     assertEquals(expected, DynamicTextReplacer.debugHelpMessage(text, f3, key));
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void debugCharts(String expected, String text, String f3, String chart1, String chart2, String chart3)
+  {
+    assertEquals(expected, DynamicTextReplacer.debugCharts(text, f3, chart1, chart2, chart3));
   }
 }
