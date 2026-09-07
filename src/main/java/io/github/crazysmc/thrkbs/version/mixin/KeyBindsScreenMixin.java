a49d547da9aa7e5c0c9870eb2f546d9e493722ea
smc <crazysmc@users.noreply.github.com>
2026-09-07 12:23:22 +0200

    allow mouse to unset keybindings

--- src/main/java/io/github/crazysmc/thrkbs/version/mixin/KeyBindsScreenMixin.java
+++ src/main/java/io/github/crazysmc/thrkbs/version/mixin/KeyBindsScreenMixin.java
@@ -1,25 +1,41 @@
 package io.github.crazysmc.thrkbs.version.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 import net.minecraft.client.input.MouseButtonEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.mojang.blaze3d.platform.InputConstants.UNKNOWN;
 
 @Mixin(KeyBindsScreen.class)
 public abstract class KeyBindsScreenMixin
 {
+  @ModifyArg(
+      method = "mouseClicked",
+      at = @At(
+          value = "INVOKE",
+          target = "Lnet/minecraft/client/KeyMapping;setKey(Lcom/mojang/blaze3d/platform/InputConstants$Key;)V"
+      )
+  )
+  private InputConstants.Key mouseClicked_setKey(InputConstants.Key key, @Local(argsOnly = true) MouseButtonEvent event)
+  {
+    return event.isEscape() ? UNKNOWN : key;
+  }
+
   @Inject(
       method = "mouseClicked",
       at = @At(
           value = "INVOKE",
-          target = "Lnet/minecraft/client/gui/screens/options/OptionsSubScreen;mouseClicked(Lnet/minecraft/client/input/MouseButtonEvent;Z)Z"
+          target = "Lnet/minecraft/client/gui/screens/options/OptionsSubScreen;mouseClicked" +
+              "(Lnet/minecraft/client/input/MouseButtonEvent;Z)Z"
       ),
       cancellable = true
   )
-  private void mouseClicked(MouseButtonEvent event, boolean doubleClick,
-                            CallbackInfoReturnable<Boolean> cir)
+  private void mouseClicked_super(MouseButtonEvent event, boolean doubleClick,
+                                  CallbackInfoReturnable<Boolean> cir)
   {
     KeyBindsScreen screen = (KeyBindsScreen) (Object) this;
     if (event.isEscape() && screen.shouldCloseOnEsc())
